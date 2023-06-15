e91258001359ffac5484b85bed84c6d92c053105
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2023-06-15 13:47:20 +0600

    started project

---
+++ src/components/Counter.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import  classes from './Counter.module.scss';
+import './index.scss';
+
+export const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count+1)
+    }
+    
+    return (
+        <div>
+            <div>{count}</div>
+            <button
+                className={classes.btn}
+                onClick={increment}>increment</button>
+        </div>
+    )
+}
